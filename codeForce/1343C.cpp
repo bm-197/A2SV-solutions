6bd9ad87721873f890cb53214c4e2d4c63570931
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-05-25 15:28:37 +0300

    Solved: C - Alternating Subsequence

--- codeForce/1343C.cpp
+++ codeForce/1343C.cpp
@@ -1,8 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     int t;
     cin >> t;
     
