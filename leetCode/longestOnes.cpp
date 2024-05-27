246a93bbc1df070bd3b27717b614922c77f120b9
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-05-27 18:09:59 +0300

    Solved: Max Consecutive Ones III

--- leetCode/longestOnes.cpp
+++ leetCode/longestOnes.cpp
@@ -8,17 +8,21 @@ using namespace std;
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        return 6;
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        int maxOnes = 0, ones=0, zeros = 0, l = 0, r = 0;
+        while (r < nums.size()) {
+            if (nums[r] == 0) zeros++;
+            if (zeros > k) {
+                while (nums[l] != 0) l++;
+                l++, zeros--;
+            }
+            ones = r - l + 1;
+            maxOnes = max(maxOnes, ones);
+            r++;
+        }
+
+        return maxOnes;
     }
 };
-
-int main() {
-    Solution sol;
-
-    vector<int> nums = {1,1,1,0,0,0,1,1,1,1,0};
-    int k = 2;
-
-    cout << sol.longestOnes(nums, k) << endl;
-
-    return 0;
-}
