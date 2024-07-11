8c1f74ecbe33f1ce4d04e8dad1b2f510ec140946
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-07-11 18:12:29 +0300

    Solved: 1929. Concatenation of Array

--- leetCode/getConcatenation.cpp
+++ leetCode/getConcatenation.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-    int n =  nums.size();    vector <int> ans(2 * n);
-        for (int i = 0; i < n; i++){
-        ans [i] = nums [i];
-        ans [i + n] = nums [i];
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+
+        int s = nums.size();
+
+        for (int i = 0; i < s; ++i) {
+            nums.push_back(nums[i]);
         }
-        return ans;
+        return nums;
     }
 };
